442438c2581dc1fc026996a05eff3818bc421b93
Flavio Ferreira <flaviometalvale@gmail.com>
2016-08-29 01:48:39 -0300

    Copy Special

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -6,54 +6,46 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
+# Problem description:
+# https://developers.google.com/edu/python/exercises/copy-special
+
+
 import sys
 import re
 import os
 import shutil
-import subprocess
-
-"""Copy Special exercise
-"""
+import zipfile
 
 
-# +++your code here+++
-# Write functions and modify main() to call them
+def get_special_paths(dir):
+    absolute_paths = []
+    files = os.listdir(dir)
 
-# LAB(begin solution)
-def get_special_paths(dirname):
-    """Given a dirname, returns a list of all its special files."""
-    result = []
-    paths = os.listdir(dirname)  # list of paths in that dir
-    for fname in paths:
-        match = re.search(r'__(\w+)__', fname)
+    for file_name in files:
+        match = re.search(r'__(\w+)__', file_name)
         if match:
-            result.append(os.path.abspath(os.path.join(dirname, fname)))
-    return result
+            absolute_paths.append(os.path.abspath(os.path.join(dir, file_name)))
+
+    return absolute_paths
 
 
-def copy_to(paths, to_dir):
-    """Copy all of the given files to the given dir, creating it if necessary."""
-    if not os.path.exists(to_dir):
-        os.mkdir(to_dir)
+def copy_to(paths, dir):
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
     for path in paths:
-        fname = os.path.basename(path)
-        shutil.copy(path, os.path.join(to_dir, fname))
-        # could error out if already exists os.path.exists():
+        file_name = os.path.basename(path)
+        new_path = os.path.join(dir, file_name)
+
+        if os.path.exists(new_path):
+            os.remove(new_path)
+        shutil.copy(path, new_path)
 
 
-def zip_to(paths, zipfile):
-    """Zip up all of the given files into a new zip file with the given name."""
-    cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
-    print("Command I'm going to do:" + cmd)
-    (status, output) = subprocess.getstatusoutput(cmd)
-    # If command had a problem (status is non-zero),
-    # print its output to stderr and exit.
-    if status:
-        sys.stderr.write(output)
-        sys.exit(1)
-
-
-# LAB(end solution)
+def zip_to(paths, zip_dir):
+    with zipfile.ZipFile(zip_dir, "w", zipfile.ZIP_DEFLATED) as zf:
+        for path in paths:
+            zf.write(path, os.path.basename(path))
 
 
 def main():
@@ -85,15 +77,17 @@ def main():
         sys.exit(1)
 
     paths = []
-    for dirname in args:
-        paths.extend(get_special_paths(dirname))
+    for dir in args:
+        paths.extend(get_special_paths(dir))
+
+    # I'm always printing the paths and running the 2 methods (when set) for teaching purposes
+    print('Special paths:')
+    print('\n'.join(paths))
 
     if todir:
         copy_to(paths, todir)
-    elif tozip:
+    if tozip:
         zip_to(paths, tozip)
-    else:
-        print('\n'.join(paths))
 
 
 if __name__ == "__main__":
