5fb51e792cba727c828cc7ffda6866f6bbe2e8fa
Flavio Ferreira <flaviometalvale@gmail.com>
2016-08-29 01:31:55 -0300

    Copy Special

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -6,10 +6,6 @@
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
 
-# Problem description:
-# https://developers.google.com/edu/python/exercises/copy-special
-
-
 import sys
 import re
 import os
@@ -17,12 +13,47 @@ import shutil
 import subprocess
 
 """Copy Special exercise
-
 """
 
 
 # +++your code here+++
 # Write functions and modify main() to call them
+
+# LAB(begin solution)
+def get_special_paths(dirname):
+    """Given a dirname, returns a list of all its special files."""
+    result = []
+    paths = os.listdir(dirname)  # list of paths in that dir
+    for fname in paths:
+        match = re.search(r'__(\w+)__', fname)
+        if match:
+            result.append(os.path.abspath(os.path.join(dirname, fname)))
+    return result
+
+
+def copy_to(paths, to_dir):
+    """Copy all of the given files to the given dir, creating it if necessary."""
+    if not os.path.exists(to_dir):
+        os.mkdir(to_dir)
+    for path in paths:
+        fname = os.path.basename(path)
+        shutil.copy(path, os.path.join(to_dir, fname))
+        # could error out if already exists os.path.exists():
+
+
+def zip_to(paths, zipfile):
+    """Zip up all of the given files into a new zip file with the given name."""
+    cmd = 'zip -j ' + zipfile + ' ' + ' '.join(paths)
+    print("Command I'm going to do:" + cmd)
+    (status, output) = subprocess.getstatusoutput(cmd)
+    # If command had a problem (status is non-zero),
+    # print its output to stderr and exit.
+    if status:
+        sys.stderr.write(output)
+        sys.exit(1)
+
+
+# LAB(end solution)
 
 
 def main():
@@ -53,8 +84,16 @@ def main():
         print("error: must specify one or more dirs")
         sys.exit(1)
 
-        # +++your code here+++
-        # Call your functions
+    paths = []
+    for dirname in args:
+        paths.extend(get_special_paths(dirname))
+
+    if todir:
+        copy_to(paths, todir)
+    elif tozip:
+        zip_to(paths, tozip)
+    else:
+        print('\n'.join(paths))
 
 
 if __name__ == "__main__":
